c28651544018c7c7268478ea76648d45a1aa8218
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-13 03:13:32 +0530

    adding todos to list functionality added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "take the dogs out for a walk",
+    "exercise daily",
+    "meditation",
+  ]);
+
+  const [input, setInput] = useState("");
+
+  console.log("🙅‍♀️", input);
+
+  const addTodo = (e) => {
+    // this will fire when button is clicked
+
+    console.log("👽", e);
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <h1>Hello Peeps!!</h1>
-      <input type="text" />
-      <button>Add Todo</button>
+      <h1 className="">Hello Peeps!!</h1>
+
+      <form>
+        <input
+          className=""
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+
+      <ul className="">
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
