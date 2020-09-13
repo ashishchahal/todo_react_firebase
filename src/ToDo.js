3986792d5ad04177532da2134048a7d7624e9a70
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-13 13:28:16 +0530

    edit todo functionality added

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,28 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDo.css";
 import db from "./firebase";
+import firebase from "firebase";
 import {
   List,
   ListItem,
   ListItemText,
   ListItemAvatar,
   Button,
+  Modal,
 } from "@material-ui/core";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function ToDo(props) {
   console.log("🔥", props);
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+
+  const updateTodo = () => {
+    //update todo with the new input text
+    // to update in firebase db we use .set
+    db.collection("todos").doc(props.todo.id).set(
+      {
+        todo: input,
+        //timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+    setOpen(false);
+  };
+
   return (
-    <List className="todo__list">
-      <ListItem>
-        <ListItemAvatar></ListItemAvatar>
-        <ListItemText primary={props.todo.todo} secondary="Dummy deadline ⏰" />
-      </ListItem>
-      <DeleteForeverIcon
-        onClick={(e) => db.collection("todos").doc(props.todo.id).delete()}
-      />
-      {/* delete functionality from db */}
-    </List>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <h1>I am a modal</h1>
+          <input
+            placeholder={props.todo.todo}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button onClick={updateTodo}>Update</Button>
+        </div>
+      </Modal>
+      <List className="todo__list">
+        <ListItem>
+          <ListItemAvatar></ListItemAvatar>
+          <ListItemText
+            primary={props.todo.todo}
+            secondary="Dummy deadline ⏰"
+          />
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>Edit</button>
+        <DeleteForeverIcon
+          onClick={(e) => db.collection("todos").doc(props.todo.id).delete()}
+        />
+        {/* delete functionality from db */}
+      </List>
+    </>
   );
 }
 
